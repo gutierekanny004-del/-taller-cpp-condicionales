bb876563301a32b98541f55c20785ffc1a891939
gutierekanny004-del <gutierekanny004@gmail.com>
2026-03-03 09:45:48 -0500

    Add grading system in ejercicio_5.cpp
    Implement grading system based on input score

---
+++ ejercicio_2/ejercicio _5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    int nota;
+    std::string letra, estado;
+
+    std::cin >> nota;
+
+    if (nota >= 90) letra = "A";
+    else if (nota >= 80) letra = "B";
+    else if (nota >= 70) letra = "C";
+    else if (nota >= 60) letra = "D";
+    else letra = "F";
+
+    if (nota >= 60) estado = "Aprobado";
+    else estado = "Reprobado";
+
+    std::cout << letra << " - " << estado;
+
+    return 0;
+}
